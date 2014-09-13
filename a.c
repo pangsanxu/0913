67a6081d8c204609d4c9a7f29cfa5d50f641f980
user <user@localhost.localdomain>
2014-09-12 18:21:01 -0700

    initialized

---
+++ a.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdl.b.h>
+#include<string.h>
+int main(int argc ,char *argv[])
+{
+	printf("hello");
+
+	exit(1);
+}
---
+++ b.c
@@ -0,0 +1,8 @@
+#include"string.h"
+#include"stdlib.h"
+#include"stdio.h"
+int main(int argc ,char *argv[])
+{
+	printf("world!\n");
+	exit(1);
+}
